ac2632248d7a685e6ff60826d9a9f26c5e971c7b
Himanshu-Chauhan1 <102967623+Himanshu-Chauhan1@users.noreply.github.com>
2022-05-23 16:26:40 +0530

    awsadded

--- src/middleWare/userAuth.js
+++ src/middleWare/userAuth.js
@@ -4,55 +4,55 @@ const booksModel = require('../models/booksModel');
 
 //----------------------------------------authentication----------------------------------------------------*/
 
-const authentication = async(req,res,next) =>{
-    try {
-        let token = req.headers["x-api-key"];
-        if (!token) token = req.headers["x-Api-Key"];
+const authentication = async(req, res, next) => {
+        try {
+            let token = req.headers["x-api-key"];
+            if (!token) token = req.headers["x-Api-Key"];
 
-        if (!token) return res.status(400).send({ status: false, massage: "token must be present" });
+            if (!token) return res.status(400).send({ status: false, message: "token must be present" });
 
-        let decodedToken = jwt.verify(token, "group_31_functionUp", {ignoreExpiration: true});
+            let decodedToken = jwt.verify(token, "group_31_functionUp", { ignoreExpiration: true });
 
-        if (!decodedToken){
-            return res.status(400).send({ status: false, massage: "token is invalid" })
-        }       
+            if (!decodedToken) {
+                return res.status(400).send({ status: false, massage: "token is invalid" })
+            }
 
-        let exp = decodedToken.exp
-        let iatNow = Math.floor(Date.now() / 1000)
-        if(exp<iatNow) {
-            return res.status(401).send({status:false,massage:'session expired, please login again'})
+            let exp = decodedToken.exp
+            let iatNow = Math.floor(Date.now() / 1000)
+            if (exp < iatNow) {
+                return res.status(401).send({ status: false, massage: 'session expired, please login again' })
+            }
+
+            req.decodedToken = decodedToken;
+
+            next()
+
+        } catch (err) {
+            console.log(err.massage)
+            res.status(500).send({ status: false, massage: err.message })
         }
-
-        req.decodedToken = decodedToken;
-
-        next()
-
-    }catch (err) {
-        console.log(err.massage)
-        res.status(500).send({ status: false, massage: err.message })
     }
-}
-//--------------------------------------authentication end----------------------------------------------------*/
+    //--------------------------------------authentication end----------------------------------------------------*/
 
 //----------------------------------------authorization----------------------------------------------------*/
 
-let authorization = async (req, res, next) => {
-    try{
-        
+let authorization = async(req, res, next) => {
+    try {
+
         let bookId = req.params.bookId
         const decodedToken = req.decodedToken
-      
-          if(!bookId){
+
+        if (!bookId) {
             return res.status(400).send({ status: false, message: 'book Id is must be present !!!!!!!' });
 
-        } else if(mongoose.Types.ObjectId.isValid(bookId) == false) {
+        } else if (mongoose.Types.ObjectId.isValid(bookId) == false) {
             return res.status(400).send({ status: false, message: "book id  is not valid !!!!!!" });
 
         }
 
-        let bookById = await booksModel.findOne({_id: bookId,isDeleted: false})
+        let bookById = await booksModel.findOne({ _id: bookId, isDeleted: false })
 
-        if(!bookById){
+        if (!bookById) {
             return res.status(404).send({ status: false, message: 'book Id is not found  !!!!!!!' });
 
         } else if (decodedToken.userId != bookById.userId) {
@@ -61,9 +61,9 @@ let authorization = async (req, res, next) => {
         }
         next();
 
-    }catch(err){
+    } catch (err) {
         console.log(err.massage)
-        res.status(500).send({status: false, massage: err.massage})
+        res.status(500).send({ status: false, massage: err.massage })
     }
 }
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,29 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const aws = require("aws-sdk")
 
 
-const {createBooks,GetFilteredBook,getBooksById,updateByBookId,deleteBooksBYId}
-= require("../controllers/booksController")
-const {createUser,loginUser} = require("../controllers/userController")
-const {authentication,authorization} = require("../middleWare/userAuth")
-const {createReview,updateReview, deleteReviewByBookIdAndReviewById} =require('../controllers/reviewController');
+const { createBooks, GetFilteredBook, getBooksById, updateByBookId, deleteBooksBYId } = require("../controllers/booksController")
+const { createUser, loginUser } = require("../controllers/userController")
+const { authentication, authorization } = require("../middleWare/userAuth")
+const { createReview, updateReview, deleteReviewByBookIdAndReviewById } = require('../controllers/reviewController');
 
 
- // User routes
- router.post('/register', createUser);
- router.post('/login', loginUser);
+// User routes
+router.post('/register', createUser);
+router.post('/login', loginUser);
 
 //book routes
-router.post('/books',authentication,createBooks);
+router.post('/books', authentication, createBooks);
 router.get('/books', authentication, GetFilteredBook);
 router.get('/books/:bookId', authentication, getBooksById);
 router.put('/books/:bookId', authentication, authorization, updateByBookId);
 router.delete('/books/:bookId', authentication, authorization, deleteBooksBYId);
 
 // Review routes
-router.post('/books/:bookId/review',createReview)
+router.post('/books/:bookId/review', createReview)
 router.put('/books/:bookId/review/:reviewId', updateReview);
-router.delete('/books/:bookId/review/:reviewId',deleteReviewByBookIdAndReviewById );
+router.delete('/books/:bookId/review/:reviewId', deleteReviewByBookIdAndReviewById);
 
-module.exports =  router;
+//aws.........................................................
+aws.config.update({
+    accessKeyId: "AKIAY3L35MCRVFM24Q7U",
+    secretAccessKey: "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",
+    region: "ap-south-1"
+})
 
+let uploadFile = async(file) => {
+    return new Promise(function(resolve, reject) {
+
+        let s3 = new aws.S3({ apiVersion: "2006-03-01" })
+
+        var uploadParams = {
+            ACL: "public-read",
+            Bucket: "classroom-training-bucket",
+            Key: "Himanshu/" + file.originalname,
+            Body: file.buffer
+        }
+
+        s3.upload(uploadParams, function(err, data) {
+            if (err) {
+                return reject({ "error": err })
+            }
+
+            console.log(data)
+            console.log(" file uploaded succesfully ")
+            return resolve(data.Location)
+        })
+
+
+    })
+}
+
+router.post("/write-file-aws", async function(req, res) {
+    try {
+        let files = req.files
+        if (files && files.length > 0) {
+
+            let uploadedFileURL = await uploadFile(files[0])
+            res.status(201).send({ msg: "file uploaded succesfully", data: uploadedFileURL })
+        } else {
+            res.status(400).send({ msg: "No file found" })
+        }
+    } catch (err) {
+        res.status(500).send({ msg: err })
+    }
+})
+
+
+module.exports = router;
